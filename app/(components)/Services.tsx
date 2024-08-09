7d23c597086fab969801ffaa2fa2b5a04f133359
Asfand Yar <asfandyar687@gmail.com>
2024-08-10 00:51:41 +0500

    feat: completed services section

--- app/(components)/Services.tsx
+++ app/(components)/Services.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div className='max-w-[1300px] mx-auto mb-44'>
+    <div className='max-w-[1300px] mx-auto mt-36 flex items-center gap-20'>
       <article className='relative'>
         <div className='relative z-[3]'>
           <Image
             src="/woman.png"
             alt="Woman"
-            width={600}
+            width={650}
             height={800}
-            className="max-w-[600px]"
+            className="max-w-[650px]"
           />
           {/* <div className='h-20 w-20 rounded-full animate-pulseGrow bg-red-400 absolute top-1/2 -translate-y-1/2 left-[23%]'></div> */}
-          <div className="bg-white p-4 rounded-full absolute top-1/2 -translate-y-1/2 left-[21%] -translate-x-[21%] z-20 animate-pulse">
+          <div className="bg-white p-4 rounded-full absolute top-1/2 -translate-y-1/2 left-[25%] -translate-x-[25%] z-20 animate-pulse">
             <Image
               src="/play.svg"
               alt="Play"
@@ -23,7 +23,7 @@ const Services = () => {
             />
           </div>
         </div>
-        <div className="max-w-[500px] absolute -bottom-20 -left-32 z-[0]">
+        <div className="max-w-[500px] absolute -bottom-20 -left-36 z-[0]">
           <Image
             src="/img-bg1.png"
             alt="Image Bg"
@@ -32,7 +32,46 @@ const Services = () => {
           />
         </div>
       </article>
-
+      <article className="flex-1">
+        <div className='flex flex-col gap-2 pb-16'>
+          <h2 className='text-[#ea395f] tracking-widest pb-4 font-bold'>OUR SERVICES</h2>
+          <h2 className='text-5xl font-bold max-w-lg leading-[4rem]'>Business Goals Achieved with Design</h2>
+        </div>
+        <div className='flex flex-col gap-8'>
+          <article className='flex gap-4'>
+            <div>
+              <Image
+                src="/subscription.svg"
+                width={80}
+                height={80}
+                alt="Subscription"
+              />
+            </div>
+            <div className='flex flex-col gap-4'>
+              <h2 className='text-xl font-semibold'>Smart Features</h2>
+              <p className='max-w-xs leading-loose'>
+                Get your blood tests delivered at let home collect sample from the victory of the managements. your blood tests.
+              </p>
+            </div>
+          </article>
+          <article className='flex gap-4'>
+            <div>
+              <Image
+                src="/partnership.svg"
+                width={80}
+                height={80}
+                alt="Partnership"
+              />
+            </div>
+            <div className='flex flex-col gap-4'>
+              <h2 className='text-xl font-semibold'>Secure Contents</h2>
+              <p className='max-w-xs leading-loose'>
+                Get your blood tests delivered at let home collect sample from the victory of the managements. your blood tests.
+              </p>
+            </div>
+          </article>
+        </div>
+      </article>
     </div>
   )
 }
