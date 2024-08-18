bc20d056ba747d8cdfef557cd74f604df381eee6
Asfand Yar <asfandyar687@gmail.com>
2024-08-19 02:20:26 +0500

    feat: added button group

--- app/(components)/subcomponents/ButtonGroup.tsx
+++ app/(components)/subcomponents/ButtonGroup.tsx
@@ -1,9 +1,28 @@
+import Image from 'next/image';
 import React from 'react'
 
-const ButtonGroup = () => {
+const ButtonGroup = ({ next, previous, goToSlide, ...rest }: Record<string, any>) => {
+  const { carouselState: { currentSlide } } = rest;
   return (
-    <div>ButtonGroup</div>
-  )
+    <div className="w-full flex justify-center md:hidden carousel-button-group absolute">
+      <button disabled={currentSlide === 0} className={currentSlide === 0 ? 'opacity-70' : ''} onClick={() => previous()}>
+        <Image
+          src="/arrow-left.svg"
+          width={60}
+          height={20}
+          alt="prev"
+        />
+      </button>
+      <button className="mr-8" onClick={() => next()}>
+        <Image
+          src="/arrow-right.svg"
+          width={60}
+          height={20}
+          alt="prev"
+        />
+      </button>
+    </div>
+  );
 }
 
 export default ButtonGroup
